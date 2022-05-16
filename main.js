6561616c73ea65e15e2ee92b5639e8d21fe71c09
kristenli <kyli2005@gmail.com>
2022-05-16 12:21:53 -0400

    commit tree 5cd3262832a47d867f194fdffd560c786f3adad4

--- main.js
+++ main.js
@@ -3,9 +3,47 @@ const EMPTY_HEART = '♡'
 const FULL_HEART = '♥'
 
 // Your JavaScript code goes here!
+let modal = document.querySelector('.hidden')
+const errorModal = document.querySelector("#modal")
 
+document.addEventListener("DOMContentLoaded", () => {
+  console.log("DOM CONTENT HAS LOADED")
+  errorModal.classList.add('hidden')
 
+clickListener()
 
+})
+
+function hideError(){
+  errorModal.classList.add('hidden')
+}
+function clickListener(){
+  document.addEventListener('click', (event) => {
+
+    if(event.target.classList[0] === 'like-glyph'){
+
+      mimicServerCall()
+      .then((response) => {
+        const activated = event.target.classList.contains("activated-heart");
+        if(activated){
+          event.target.classList.remove("activated-heart")
+          event.target.innerHTML = EMPTY_HEART
+        }else{
+          event.target.classList.add("activated-heart")
+          event.target.innerHTML = FULL_HEART
+        }
+        activated;
+      })
+      .catch((error) => {
+        console.log(error);
+        errorModal.remove("hidden");
+        setTimeout(() => {
+          hideError()
+         }, 3000);
+      });
+    }
+  });
+}
 
 //------------------------------------------------------------------------------
 // Don't change the code below: this function mocks the server response
